6c547e005ca0b20732bcaa377e1cff6f03e21133
Kieran Simpson <kierans777@gmail.com>
2018-05-09 18:40:36 +1000

    Updated uploader to not upload if driver already exists in S3

--- uploader/uploader.js
+++ uploader/uploader.js
@@ -13,8 +13,7 @@ AWS.config.update({
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 const DEFAULT_PARAMS = {
-  Bucket: "nw-chromedriver",
-  ContentType: "application/octet-stream"
+  Bucket: "nw-chromedriver"
 };
 
 /**
@@ -25,27 +24,17 @@ const DEFAULT_PARAMS = {
  * @returns {Promise}
  */
 module.exports = function(version, platform, arch, filepath) {
-  return new Promise(function(resolve, reject) {
-    const params = clone(DEFAULT_PARAMS, false);
-    params.Key = makeKey(version, platform, arch);
-    params.Body = fs.createReadStream(filepath);
+  const params = clone(DEFAULT_PARAMS, false);
+  params.Key = makeKey(version, platform, arch);
 
-    determineFileSize(filepath)
-      .then(function(size) {
-        params.ContentLength = size;
-      })
-      .then(function() {
-        console.log("Uploading '" + params.Key + "'");
+  return checkIfFileAlreadyExists(params)
+    .then(function(exists) {
+      if (!exists) {
+        return uploadFile(params, filepath)
+      }
 
-        s3.putObject(params, function(err, data) {
-          if (err) {
-            return reject(err);
-          }
-
-          resolve(data);
-        });
-      });
-  });
+      console.log("'" + params.Key + "' already exists");
+    });
 };
 
 function makeKey(version, platform, arch) {
@@ -64,4 +53,44 @@ function determineFileSize(filepath) {
       resolve(stats.size);
     })
   })
+}
+
+function checkIfFileAlreadyExists(params) {
+  return new Promise(function(resolve, reject) {
+    s3.headObject(params, function(err) {
+      if (err) {
+        if (err.code === "NotFound") {
+          return resolve(false);
+        }
+
+        return reject(err);
+      }
+
+      return resolve(true);
+    })
+  })
+}
+
+function uploadFile(params, filepath) {
+  return determineFileSize(filepath)
+    .then(function(size) {
+      params.ContentType = "application/octet-stream";
+      params.ContentLength = size;
+      params.Body = fs.createReadStream(filepath);
+    })
+    .then(putDriverIntoBucket.bind(null, params));
+}
+
+function putDriverIntoBucket(params) {
+  return new Promise(function(resolve, reject) {
+    console.log("Uploading '" + params.Key + "'");
+
+    s3.putObject(params, function(err, data) {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(data);
+    });
+  });
 }
