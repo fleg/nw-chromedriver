e9d9e0bbcdd9bfcfb6cd134b707fa99a34d0df02
Kieran Simpson <kierans777@gmail.com>
2018-05-11 23:19:45 +1000

    Filter out versions less than the min version.

--- nwscrapper/main.js
+++ nwscrapper/main.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const cheerio = require("cheerio");
+const semver = require("semver");
 
 const scrapper = require("./scrapper");
 
 const RELEASE = /^v\d+\.\d+\.\d+\/$/;
+const MIN_VERSION = "0.13.0";
 
 scrapper("http://dl.nwjs.io/")
   .then(function(body) {
@@ -20,6 +22,10 @@ scrapper("http://dl.nwjs.io/")
 
     versions = versions.map(function(version) {
       return version.replace("v", "").replace("/", "");
+    });
+
+    versions = versions.filter(function(version) {
+      return semver.gte(version, MIN_VERSION);
     });
 
     versions.forEach(function(version) {
