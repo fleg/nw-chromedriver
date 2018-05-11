8ab0eb254774c7a56ca83bb94488c3f608bb5e05
Kieran Simpson <kierans777@gmail.com>
2018-05-11 23:15:12 +1000

    Parse download list for versions.

--- nwscrapper/main.js
+++ nwscrapper/main.js
@@ -1,8 +1,28 @@
 "use strict";
 
+const cheerio = require("cheerio");
+
 const scrapper = require("./scrapper");
+
+const RELEASE = /^v\d+\.\d+\.\d+\/$/;
 
 scrapper("http://dl.nwjs.io/")
   .then(function(body) {
-    console.log(body);
+    const $ = cheerio.load(body);
+
+    var versions = $("a").map(function(i, link) {
+      return $(link).text();
+    }).get();
+
+    versions = versions.filter(function(version) {
+      return RELEASE.test(version);
+    });
+
+    versions = versions.map(function(version) {
+      return version.replace("v", "").replace("/", "");
+    });
+
+    versions.forEach(function(version) {
+      console.log(version);
+    })
   });
